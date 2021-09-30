061dae724499a07c408ce7e3bb2cf8fa1b1bfe87
spaceforestchu <spaceforestchu@gmail.com>
2021-09-30 15:56:28 -0400

    second

--- routes/users/controller/userController-v1.js
+++ routes/users/controller/userController-v1.js
@@ -1,49 +1,82 @@
 const bcrypt = require("bcryptjs");
 
-const {
-  isEmpty,
-  isAlpha,
-  isAlphanumeric,
-  isEmail,
-  isStrongPassword,
-} = require("validator");
-
 const User = require("../model/User");
+
+function checkForNumberAndSymbol(target) {
+  if (target.match(/[!`\-=@#$%^&*()\[\],.?":;{}|<>1234567890]/g)) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+function checkIsEmpty(target) {
+  if (target.length === 0) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+function checkSymbol(target) {
+  if (target.match(/[!`\-=@#$%^&*()\[\],.?":;{}|<>]/g)) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+function checkIsEmail(target) {
+  if (target.match(/\S+@\S+\.\S+/)) {
+    return false;
+  } else {
+    return true;
+  }
+}
+
+function checkPaswordStrength(target) {
+  var strongRegex = new RegExp(
+    "^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[_!@#$%^=-{}[]&*|:;'?.<>`~])(?=.{8,})"
+  );
+  return !strongRegex.test(target);
+}
 
 async function createUser(req, res) {
   const { firstName, lastName, username, email, password } = req.body;
   let body = req.body;
+  //How would you validate firstName to make sure only alphabet is allowed
   let errObj = {};
 
   for (let key in body) {
-    if (isEmpty(body[key])) {
+    if (checkIsEmpty(body[key])) {
       errObj[`${key}`] = `${key} cannot be empty`;
     }
   }
 
-  if (!isAlpha(firstName)) {
+  if (checkForNumberAndSymbol(firstName)) {
     errObj.firstName = "First Name cannot have special characters or numbers";
   }
 
-  if (!isAlpha(lastName)) {
-    errObj.lastName = "Last Name cannot have special characters or numbers";
+  if (checkForNumberAndSymbol(lastName)) {
+    errObj.lastName = "last Name cannot have special characters or numbers";
   }
 
-  if (!isAlphanumeric(username)) {
+  if (checkSymbol(username)) {
     errObj.username = "Username cannot have special characters";
   }
 
-  if (!isEmail(email)) {
+  if (checkIsEmail(email)) {
     errObj.email = "please enter a valid email";
   }
 
-  if (!isStrongPassword(password)) {
+  //min 8 characters - alphabet, number, special number, one uppercase
+  console.log(checkPaswordStrength(password));
+  if (checkPaswordStrength(password)) {
     errObj.password =
       "Your password must contain 1 lowercase, 1 uppercase, 1 number, 1 special character and at least 8 characters long";
   }
 
   if (Object.keys(errObj).length > 0) {
-    //How would you validate firstName to make sure only alphabet is allowed
     return res.status(500).json({
       message: "error",
       error: errObj,
@@ -68,13 +101,6 @@ async function createUser(req, res) {
   } catch (error) {
     res.status(500).json({ message: "error", error: error.message });
   }
-}
-
-async function login(req, res) {
-  //log the user in using email and password
-  //if email does not exists, error message "please go sign up"
-  //if email exists but wrong password error message "please check your email and password"
-  //if successful - for now send message "Login success"
 }
 
 module.exports = {
